5bc4d605669e0f59a94b3fccac2eb904ab99c0ce
Puspak Sengupta <puspaksg1994@gmail.com>
2024-06-29 12:50:54 +0530

    Second Commit

---
+++ src/arrays/IntersectionofTwoArrays.java
@@ -0,0 +1,47 @@
+package arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntersectionofTwoArrays {
+
+    public static void main(String[] args) {
+
+        int arr1[] = {1,2,2,3,5,6,7};
+        int arr2[] = {1,3,4,5,6,7};
+
+        int intersection[] = interofTwoArrays(arr1,arr2);
+
+        System.out.print("The result is: ");
+
+        for(int num: intersection){
+            System.out.print(num + " ");
+        }
+    }
+
+    private static int[] interofTwoArrays(int[] arr1, int[] arr2) {
+
+        List<Integer> intersectionList = new ArrayList<>();
+        int i=0;
+        int j=0;
+
+        while(i<arr1.length && j<arr2.length){
+            if(arr1[i] == arr2[j]){
+                intersectionList.add(arr1[i]);
+                i++;
+                j++;
+            }else if(arr1[i] < arr2[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+
+        int[] intersection = new int[intersectionList.size()];
+        for(int n=0;n<intersectionList.size();n++){
+            intersection[n] = intersectionList.get(n);
+        }
+
+        return intersection;
+    }
+}
